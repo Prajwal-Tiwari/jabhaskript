1d99377d6cf5ed6ebfc6378e3e631880338dff7c
Prajwal Tiwari <tiwariprajwal705@gmail.com>
2026-07-14 00:39:35 +0530

    Promise learned

--- Adv JS/promise.js
+++ Adv JS/promise.js
@@ -19,38 +19,86 @@
 //     console.log("resolved");
 // });
 
-const promisetwo = new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-       let user = {username: "Prajwal", email: "prajwal@gmail.com"};
-       let error = false;
-       if(error){
-        reject("Something gone wrong....");
-       }
-       else{
-        resolve(user);
-       }
-    },1000);
+// const promisetwo = new Promise((resolve, reject)=>{
+//     setTimeout(()=>{
+//        let user = {username: "Prajwal", email: "prajwal@gmail.com"};
+//        let error = false;
+//        if(error){
+//         reject("Something gone wrong....");
+//        }
+//        else{
+//         resolve(user);
+//        }
+//     },1000);
     
+// })
+// .then((user)=>{
+//     console.log(user);
+//     return user.username;
+// })
+// .then((usrnme)=>{
+//     console.log(usrnme);
+//     return usrnme*2;
+// })
+// .then((usr)=>{
+//     console.log(usr);
+//     return 45;
+// })
+// .then((hi)=>{
+//     console.log(hi);
+// })
+// .then((wht)=>{
+//     console.log(wht);
+// })
+// .catch((error)=>{
+//     console.log(error);
+// })
+// .finally(()=>{console.log("The promise exists")});
+
+
+const promiseThree = new Promise((res, rej)=>{
+    setTimeout(function(){
+        let error = false;
+        if(!error){
+            res({username: "Prajwal3", password: "123"});
+        } else{
+            rej("Error: JS went wrong3");
+        }
+    },1000)
 })
-.then((user)=>{
-    console.log(user);
-    return user.username;
+
+
+
+async function consumepromiseThree(){
+    try{
+        const resp = await promiseThree;
+        console.log(resp);
+    } catch(error){
+        console.log(error);
+    }
+}
+
+consumepromiseThree();
+
+
+// async function promiseFour(){
+//     try{
+//         const response = await fetch('https://jsonplaceholder.typicode.com/users');
+//         const data = await response.json();
+//         console.log(data);
+        
+//     } catch(error){
+//         console.log("Error: ", error);
+//     }
+// }
+
+// promiseFour();
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then((response)=>{
+    return response.json();
 })
-.then((usrnme)=>{
-    console.log(usrnme);
-    return usrnme*2;
+.then((data)=>{
+    console.log(data);
 })
-.then((usr)=>{
-    console.log(usr);
-    return 45;
-})
-.then((hi)=>{
-    console.log(hi);
-})
-.then((wht)=>{
-    console.log(wht);
-})
-.catch((error)=>{
-    console.log(error);
-})
-.finally(()=>{console.log("The promise exists")});
+.catch((error)=>{console.log(error)});
